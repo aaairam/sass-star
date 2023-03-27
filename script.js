11eae321aef60103e2124cee06a46742b70ca316
aaairam <magildaceria@gmail.com>
2023-03-27 15:36:43 +0800

    update

--- script.js
+++ script.js
@@ -5,3 +5,49 @@ dropDown.forEach((dropDowns) => {
         dropDowns.querySelector("i").classList.toggle("open");
     })
 })
+
+const toggleMenu = document.querySelector(".toggle__menu");
+const blur = document.querySelector(".background");
+const headerNav = document.querySelector(".header__mobile__nav");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  blur.classList.toggle("open");
+  headerNav.classList.toggle("open");
+});
+
+const starNav=document.querySelectorAll(".star__nav__link");
+const starContent=document.querySelectorAll(".coffee-tab_content");
+starNav.forEach((star) => {
+  star.addEventListener("click", () => {
+    removeActiveStar();
+    star.classList.add("active");
+    const activeContent= document.querySelector(`#${star.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+  })
+})
+
+function removeActiveStar(){
+  starNav.forEach((star) =>{
+    star.classList.remove("active");
+  })
+}
+function removeActiveContent(){
+  starContent.forEach((star) =>{
+    star.classList.remove("active");
+  })
+}
+
+
+const btop = document.querySelector(".backtotop");
+window.addEventListener('scroll', () => {
+  console.log(window.pageYOffset);
+  if  (window.pageYOffset >= 100) {
+    btop.classList.add('show')
+  }else {
+    btop.classList.remove('show')
+  }
+});
+
+
+
